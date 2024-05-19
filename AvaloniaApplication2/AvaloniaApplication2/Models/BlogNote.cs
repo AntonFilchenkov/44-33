ffb9c6ee454180256475bd3ebfe0fd7df5cbe69d
AntonFilchenkov <0808anfil@gmail.com>
2024-05-19 22:40:23 +0300

    d аук

--- AvaloniaApplication2/AvaloniaApplication2/Models/BlogNote.cs
+++ AvaloniaApplication2/AvaloniaApplication2/Models/BlogNote.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using System;
 using System.Collections.Generic;
+using System.Formats.Asn1;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +11,17 @@ namespace AvaloniaApplication2.Models
 {
     public class BlogNote
     {
-        public BlogNote(string title_1, string content, string imageurl = "")
+        public BlogNote(string title_1, string content, List<string> tags, string imageurl = "")
         {
             Title_1 = title_1;
             Content = content;
-            ImageURL = imageurl;
+            Image = new Bitmap(AssetLoader.Open(new Uri(imageurl)));
+            Tags = tags;
         }
         public string Title_1 { get; set; }
         public string Content { get; set; }
-        public string ImageURL { get; set; }
+        public Bitmap Image { get; set; }
+        public List<string> Tags { get; set; }
+         
     }
 }
--- AvaloniaApplication2/AvaloniaApplication2/ViewModels/MainViewModel.cs
+++ AvaloniaApplication2/AvaloniaApplication2/ViewModels/MainViewModel.cs
@@ -1,9 +1,10 @@
 ﻿using AvaloniaApplication2.Models;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace AvaloniaApplication2.ViewModels;
 
-public partial class MainViewModel : ViewModelBase
+public class MainViewModel : ViewModelBase
 {
     public string Greeting => "Welcome to Avalonia!";
     public string text => "Lorem Ipsum - это текст-рыба, часто используемый в печати и вэб-дизайне.";
@@ -14,7 +15,18 @@ public partial class MainViewModel : ViewModelBase
         Articles.Add(new Article("Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться.", "12:35 04.08.23"));
         Articles.Add(new Article("Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться.", "12:35 04.08.23"));
         BlogNotes = new ObservableCollection<BlogNote>();
-        BlogNotes.Add(new BlogNote("Article 1", "Lorem Ipsum - это текст-\"рыба\", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной \"рыбой\" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ9Q9mL_NJC8lVnhIShHHATil902Kk8-cboQLpGH8IRA&s"));
+        BlogNotes.Add(new BlogNote(
+            "Article 1",
+            "Lorem Ipsum - это текст-\"рыба\", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной \"рыбой\" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.",
+            new List<string>()
+            {
+                "Tag 1",
+                "Tag 2",
+                "Tag 3"
+            },
+            "avares://AvaloniaApplication2/Assets/logo.png"
+
+            ));
         CurrentBlogNote = BlogNotes[0];    
     }
     public ObservableCollection<BlogNote> BlogNotes { get; set; }
